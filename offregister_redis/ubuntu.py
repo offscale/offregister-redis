7397a45ff0e66adc6b9fbf049deec82cd6c9fa02
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-07-03 22:45:05 +1000

    Bumped Redis version

--- offregister_redis/ubuntu.py
+++ offregister_redis/ubuntu.py
@@ -6,4 +6,4 @@ from offregister_redis.base import dl_install_redis_server
 def dl_install0(*args, **kwargs):
     apt_depends('build-essential')
     return dl_install_redis_server(listen_port=kwargs.get('redis_port', 6379),
-                                   version=kwargs.get('redis_version', '5.0.4'))
+                                   version=kwargs.get('redis_version', '5.0.5'))
